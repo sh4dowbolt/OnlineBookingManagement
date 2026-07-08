8ed22ca504e170bc720710892c2914c7d814146b
Surayev <dmit.palckin@yandex.ru>
2026-07-08 19:33:55 +0300

    syntax fix

--- src/main/java/com/suraev/OnlineBokingManagement/entities/Hour.java
+++ src/main/java/com/suraev/OnlineBokingManagement/entities/Hour.java
@@ -14,12 +14,4 @@ public enum Hour {
     Nineteen,
     Twenty
 
-    public List<Hour>  getAllHours() {
-        List<Hour> hours = new ArrayList<Hour>();
-        hours.add(Twelve);
-        hours.add(Thirteen);
-        hours.add(Fourteen);
-        hours.add(Fifteen);
-
-    }
 }
