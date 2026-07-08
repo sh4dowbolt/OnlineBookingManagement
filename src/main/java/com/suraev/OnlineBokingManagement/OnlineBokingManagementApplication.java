03045f47a1a3b1ba0e67de92ab37fd5412220d20
Surayev <dmit.palckin@yandex.ru>
2026-07-08 18:03:46 +0300

    first commit

---
+++ src/main/java/com/suraev/OnlineBokingManagement/OnlineBokingManagementApplication.java
@@ -0,0 +1,13 @@
+package com.suraev.OnlineBokingManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineBokingManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineBokingManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/suraev/OnlineBokingManagement/OnlineBokingManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suraev.OnlineBokingManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineBokingManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
